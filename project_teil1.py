1ebe07e15f5efa5c5e01aa175e3a25b02444df06
sergiusdell75 <34401493+sergiusdell75@users.noreply.github.com>
2019-12-20 09:48:43 +0100

    Add files via upload

--- project_teil1.py
+++ project_teil1.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
+# ### Segmenting and Clustering Neighborhoods in Toronto. Part 1.
+# 
+
+# In[58]:
 
 
 import requests
 import lxml.html as lh
 import pandas as pd
 
-#Create a handle, page, to handle the contents of the website
+
+# #####  Load website
+
+# In[59]:
+
+
+# Create a handle, page, to handle the contents of the website
 url='https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M'
 page = requests.get(url)#Store the contents of the website under doc
 doc = lh.fromstring(page.content)#Parse data that are stored between <tr>..</tr> of HTML
-tr_elements = doc.xpath('//tr')
-
-
-# In[7]:
-
-
-[len(T) for T in tr_elements[:12]]
-
-
-# In[9]:
-
-
 tr_elements = doc.xpath('//tr')#Create empty list
 col=[]
 i=0#For each row, store each first element (header) and an empty list
@@ -32,49 +29,91 @@ for t in tr_elements[0]:
     name=t.text_content()
     #print '%d:"%s"'%(i,name)
     col.append((name,[]))
-
-
-# In[13]:
-
-
-#Since out first row is the header, data is stored on the second row onwards
 for j in range(1,len(tr_elements)):
-    #T is our j'th row
     T=tr_elements[j]
-    
-    #If row is not of size 10, the //tr data is not from our table 
     if len(T)!=3:
         break
-    
-    #i is the index of our column
     i=0
-    
-    #Iterate through each element of the row
     for t in T.iterchildren():
         data=t.text_content() 
-        #Check if row is empty
         if i>0:
-        #Convert any numerical value to integers
             try:
                 data=int(data)
             except:
                 pass
-        #Append the data to the empty list of the i'th column
         col[i][1].append(data)
-        #Increment i for the next column
         i+=1
 
 
-# In[14]:
+# In[60]:
 
 
+# check if the data are loaded
 [len(C) for (title,C) in col]
 
 
-# In[16]:
+# ##### create  data frame
+
+# In[61]:
 
 
+#create a data frame and delete 'not assigned' from the dataframe
 Dict={title:column for (title,column) in col}
 df=pd.DataFrame(Dict)
-df.head(10)
+df.astype(str)
+indexNames = df[ df['Borough'] == 'Not assigned' ].index
+df.drop(indexNames , inplace=True)
+df.head(3)
+
+
+# In[62]:
+
+
+# remove \n from the column name and rows
+df = df.replace('\n','', regex=True)
+df.rename(columns={"Neighbourhood\n": "Neighbourhood"}, inplace=True)
+df.head(3)
+
+
+# In[63]:
+
+
+# combine multiple Neighbourhood rows into one. THEN regroup by 'Postcode' and 'Borough'
+df=df.groupby(['Postcode','Borough'])['Neighbourhood'].apply(', '.join).reset_index()
+df.head(5)
+
+
+# In[64]:
+
+
+# find the 'Not assigned' entries from the 'Neighbourhood'
+indexNames = df[ df['Neighbourhood'] == 'Not assigned']
+print(indexNames)
+
+
+# In[69]:
+
+
+# replace the 'Not assigned' entries based on the corresponding 'Borough's entries
+indexNames = df[ df['Neighbourhood'] == 'Not assigned'].index
+df.loc[indexNames,'Neighbourhood'] = df1.loc[indexNames,'Borough']
+indexNames = df[ df['Neighbourhood'] == 'Not assigned']
+print(indexNames)
+
+
+# In[66]:
+
+
+# check the Neighbourhood column
+indexNames = df[ df['Neighbourhood'] == 'Queen\'s Park']
+print(indexNames)
+
+
+# ##### Final shape
+
+# In[67]:
+
+
+# shape the data set
+df.shape
 
